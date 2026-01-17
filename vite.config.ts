9217c3ccd8fc50557f40c8cf33589741e3d3ddab
Evan Boodoosingh <evan9496@gmail.com>
2026-01-17 16:30:39 -0500

    Fix deployment: update build directory from dist to build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   base: "./",
+  build: {
+    outDir: "build",
+  },
   plugins: [react()],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
